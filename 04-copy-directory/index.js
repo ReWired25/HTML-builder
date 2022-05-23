d50530dc4b0264e4d15212ce6fc24aa0a85cbea5
Nikita K <w.feelingalright.p@yandex.ru>
2022-05-23 07:09:02 +0300

    feat: add 4 task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const { mkdir, rm, readdir, copyFile } = require('fs/promises');
 
+function copyDir(folderArg, destArg) {
+  const folder = folderArg;
+  const dest = destArg;
+
+  function copyFiles() {
+    const files = readdir(path.join(folder));
+
+    files.then(resolve => resolve.forEach(item => {
+      fs.stat(path.join(folder, item), (err, data) => {
+        if (err) throw err;
+
+        if (data.isDirectory()) {
+          copyDir(path.join(folder, item), path.join(dest, item));
+        } else {
+          copyFile(path.join(folder, item), path.join(dest, item));
+        }
+      });
+    }));
+  }
+
+  mkdir(path.join(dest), {recursive: true})
+    .then(resolve => {
+      if (!resolve) {
+        rm(path.join(dest), {recursive: true})
+          .then(() => {
+            mkdir(path.join(dest));
+            copyFiles();
+          });
+      } else {
+        copyFiles();
+      }
+    });
+}
+
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
+
+console.log('Files copied successfully!');
